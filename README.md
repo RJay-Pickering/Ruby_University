e37c5c57033fd848adea5d1c93e104d3c8e87a21
RJay-Pickering <rjaypickering@gmail.com>
2024-02-21 10:25:02 -0600

    updated the project and added a professional readme for project.

--- README.md
+++ README.md
@@ -1,21 +1,52 @@
 <div align="center">
-  <img style="width: 30%;" src="templateImage.png" alt="project image">  
+  <img style="width: 30%; margin: 0;" src="README_Files/ruby-university.png" alt="project image">  
   
-  # **Your title**
+  # **Ruby University**
 
   ## **DESCRIPTION**
-  Replace this sentence with your own description of your project.
+ This project represents a fake registration program that was created with Ruby. In this app, users engage with the program named 'Ruby Bot' to complete a mock registration process. 'Ruby Bot' determines if your eligibility for an interview based on your qualifications.
 
-  COLOR PALETTE            |  DEMO
-:-------------------------:|:-------------------------:
-<img style="width: 100%;" src="templateImage.png" alt="Color Palette">   |  <img style="width: 100%;" src="example-here-is-a-sample.gif" alt="DEMO"> 
+ ## **DEMO**
+
+<img style="width: 100%;" src="README_Files/demo.gif" alt="DEMO"> 
 </div>
 
 ## **KEY FEATURES:**
-list out what your page does and your inspiration behind your project
+1. **START**
+    - The program will start by greeting the user and ask for their name.
+    - It will then ask the user for their date of birth by year/month/day (E.X. 2024/02/16)
+    - If the user is 18 or above, they are able to proceed with the program, otherwise, it will tell the user that they are too young and close the program.
+2. **IMPORTANT VARIABLES**
+    - `disappointment` - A number variable that starts off at 0 and will count up if the user does something that their not suppose to do. This determines if the user is accepted or not.
+    - `work_experience` - A boolean variable that starts off as false and will become true if the user says that they have work experience. This is not required to be accepted.
+    - `worthy` - A boolean variable that starts off as true and will become false if the user has too many disappointment points, or if they do not list any obstacle. This must be true for the user to be accepted for a interview.
+3. **QUESTIONS**
+    - First question: `"What are your achievements?"`. 
+      - The user can list out all of their achievements that they have earned throughout their life.
+      - If the user quits without adding an achievement, then they would have a little `disappointment` added to their counter.
+    - Second question: `"Do you have any work experience?"`
+      - If the user types `yes`, then the user will have to list out their work experience. If the user does not add their work experience after quitting then they would have a little `disappointment` added to their counter.
+      - If the user types `no`, then they would proceed to the next question.
+    - Third question: `"What are your skills?"`
+      - The user can list out all of their skills that they have learned.
+      - If the user quits without adding an skill, then they would have a little `disappointment` added to their counter.
+    - Final question: `"What obstacles have you overcome?"`
+      - The user can list out all of their obstacles that they overcome.
+      - If the user quits without adding an obstacle, then they would have a little `disappointment` added to their counter and be deemed not `worthy`.
+4. **RESULTS**
+    - It will tell the user if they qualify for a interview with the university board or not.
+    - It will then tell the user their score.
+    - Then, it will list out all of the information that they provided.
+5. **INSPIRATION**
+    - My inspiration for this project was to make a small, real world application that I could make to test out my fresh skills of Ruby.
 
 ## **HOW TO USE (Visual Studio Code)**
-list out the steps of how someone could use or run the project on their own.
+1. Install [Ruby](https://www.ruby-lang.org/en/documentation/installation/) to your computer.
+2. Download [Visual Studio Code](https://www.youtube.com/watch?v=HxJXKFxhah4)
+3. Install the extensions "Ruby LSP" by Shopify and "VSCode rdbg Ruby Debugger" by Koichi Sasada.
+4. clone this repo.
+5. Open the project.
+6. type `ruby path/to/filename.rb` through the terminal to run the project.
 
 ## **LICENSE**
-the license name here
+N/A
--- main.rb
+++ main.rb
@@ -1,16 +1,23 @@
 require 'date'
 
 class Acceptance
-    attr_accessor :dissapointment, :work_experience, :worthy
+    attr_accessor :disappointment, :work_experience, :worthy
 end
 
 def main
     acceptance = Acceptance.new()
-    acceptance.dissapointment = 0
+    acceptance.disappointment = 0
     # true means that the user has work experience while false is the opposite!
     acceptance.work_experience = false
     # true means that the user is worthy for an interview while false is the opposite!
     acceptance.worthy = true
+
+    d = DateTime.now
+    today = d.strftime("%Y%m%d")
+    achev = Array.new
+    exp = Array.new
+    skil = Array.new
+    obst = Array.new
 
     puts "Welcome To Ruby University!"
     puts "I am RubyBot, and I am here to assist you in your application process!"
@@ -25,47 +32,42 @@ def main
     puts "Well #{name}, what is your date of birth? (EX: Year/Month/Day)"
 
     birth_date = gets.chomp
-
-    d = DateTime.now
-    today = d.strftime("%Y%m%d")
     birth = birth_date.tr('/','')
     years = (today.to_i - birth.to_i) / 10000
-    achev = Array.new
-    exp = Array.new
-    skil = Array.new
-    obst = Array.new
 
-    if years < 17
-        puts "Your not old enough #{name}, try again another time!"
+    # /////////////////////////////////clearing terminal////////////////////////////////
+    system("clear") || system("cls")
+    # /////////////////////////////end of clearing terminal/////////////////////////////
+
+    if years < 18
+        puts "Your not old enough #{name}, try again another time when your 18 or older!"
     else
         puts "So your #{years} years old..."
     # ///////////////////////////end of getting the birth year///////////////////////////
         puts ""
+        puts "You are old enough!"
+        puts "Tell me, what are your achievements? press [q] to stop listing achievements."
     # ////////////////////////////////////achevements////////////////////////////////////
         while true
-            puts "You are old enough! Tell me, what are your achevements? press [q] to stop listing achevements."
             achevements = gets.chomp
             if achevements == "q"
                 if achev.length == 0
-                    acceptance.dissapointment += 1
-                    puts "Please put at least 1 achevement!"
-                    achevements = gets.chomp
+                    acceptance.disappointment += 1
+                    puts "Oh..."
+                    break
                 else
                     break
                 end
-            end
-            while true
-                if achevements == "" or achevements == " "
-                    acceptance.dissapointment += 1
-                    puts "Please put at least 1 achevement!"
-                    achevements = gets.chomp
-                else
-                    achev.push(achevements)
-                    break
-                end
+            elsif achevements.tr(' ', '').length != 0
+                achev.push(achevements)
             end
         end
     # /////////////////////////////////end of achevements/////////////////////////////////
+
+        # /////////////////////////////////clearing terminal////////////////////////////////
+        system("clear") || system("cls")
+        # /////////////////////////////end of clearing terminal/////////////////////////////
+
         puts "Hmmm... Okay, well we will evaluate this later, do you have any work experience?"
 
     # //////////////////////////////////work experience//////////////////////////////////
@@ -77,92 +79,119 @@ def main
                 while true
                     jobs = gets.chomp
                     if jobs == "q"
-                        break
+                        if exp.length == 0
+                            acceptance.disappointment += 1
+                            break
+                        else
+                            break
+                        end
+                    elsif jobs.tr(' ', '').length != 0
+                        exp.push(jobs)
                     end
-                    exp.push(jobs)
                 end
                 break
             elsif work == "no"
                 acceptance.work_experience = false
-                puts "To bad..."
                 break
             else
+                acceptance.disappointment += 1
                 puts "Please try typing yes or no instead of #{work}!"
             end
         end
     # ///////////////////////////////end of work experience///////////////////////////////
 
-        puts "Your skills? press [q] to stop listing skills"
+        # /////////////////////////////////clearing terminal////////////////////////////////
+        system("clear") || system("cls")
+        # /////////////////////////////end of clearing terminal/////////////////////////////
+
+        if acceptance.work_experience == false
+            puts "To bad..."
+        end
+        puts "What are your skills? press [q] to stop listing skills"
 
     # ///////////////////////////////////////skills///////////////////////////////////////
-        skills = gets.chomp
         while true
-            if skills == "" or skills == " "
-                acceptance.dissapointment += 1
+            skills = gets.chomp
+            if skills == "" and skil.length == 0 or skills == " " and skil.length == 0
+                acceptance.disappointment += 1
                 puts "Please put at least 1 skill!"
                 skills = gets.chomp
             elsif skills == "q"
                 break
-            else
+            elsif skills != "" or skills != " "
                 skil.push(skills)
             end
         end
     # ///////////////////////////////////end of skills///////////////////////////////////
 
+        # /////////////////////////////////clearing terminal////////////////////////////////
+        system("clear") || system("cls")
+        # /////////////////////////////end of clearing terminal/////////////////////////////
+
         puts "One last question..."
-        puts "What obstacles that you have overcome!"
+        puts "What obstacles have you overcome? press [q] to stop listing obstacles"
 
     # /////////////////////////////////////obstacles/////////////////////////////////////
-        obstacles = gets.chomp
         while true
-            if obstacles == "" or obstacles == " "
+            obstacles = gets.chomp
+            if obstacles == "" or obstacles == " " or obstacles == "none" or obstacles == "None"
                 puts "Please put a valid obstacle!"
-            elsif (obstacles == "none" and obst.length == 0) or (obstacles == "Have none" and obst.length == 0) 
-                acceptance.dissapointment += 1
-                acceptance.worthy = false
-                puts "Oh..."
-                break
             elsif obstacles == "q"
-                puts "Wonderful!"
-                break
+                if obst.length == 0
+                    acceptance.disappointment += 1
+                    acceptance.worthy = false
+                    puts "Oh..."
+                    break
+                else
+                    puts "Wonderful!"
+                    break
+                end
             else
                 obst.push(obstacles)
             end
         end
     # //////////////////////////////////end of obstacles//////////////////////////////////
 
-        if acceptance.dissapointment >= 3
+        if acceptance.disappointment >= 2
             acceptance.worthy = false
         end
 
-        next_month = d.next_month.strftime("%d/%m/%Y")
+        next_month = d.next_month.strftime("%m/%d/%Y")
+
+        # /////////////////////////////////clearing terminal////////////////////////////////
+        system("clear") || system("cls")
+        # /////////////////////////////end of clearing terminal/////////////////////////////
 
     # //////////////////////////////////accepted or not//////////////////////////////////
         score = Random.new
-        while true
-            if acceptance.worthy == false
-                puts "You have been declined an interview with the board... please try again next year!"
-                results = score.rand(0..59) 
-                puts "Your total score is #{results}!"
-                break
-            elsif acceptance.dissapointment == 0
-                puts "You passed! You will have an interview on #{next_month}, good luck!   =)"
-                results = score.rand(80..100) 
-                puts "Your total score is #{results}!"
-                break
-            else
-                puts "You barely passed! You will have an interview on #{next_month}, good luck!   =)"
-                results = score.rand(60..79)
-                puts "Your total score is #{results}!"
-                break
-            end
+        if acceptance.worthy == false
+            puts "You have been declined an interview with the board... please try again next year!"
+            results = score.rand(0..59) 
+            puts "Your total score is #{results}!"
+        elsif acceptance.disappointment == 0
+            puts "You passed! You will have an interview on #{next_month}, good luck!   =)"
+            results = score.rand(80..100) 
+            puts "Your total score is #{results}!"
+        else
+            puts "You barely passed! You will have an interview on #{next_month}, good luck!   =)"
+            results = score.rand(60..79)
+            puts "Your total score is #{results}!"
         end
     # ///////////////////////////////end of accepted or not///////////////////////////////
-
-    puts "Heres a list of your achevements: #{achev}"
-    puts "Heres a list of your experience: #{exp}"
-    puts "Heres a list of your skills: #{skil}"
-    puts "Heres a list of your obstacles: #{obst}"
+        if achev.length != 0
+           puts "Heres a list of your achievements: #{achev}"
+        end
+        if acceptance.work_experience == true and exp.length != 0
+            puts "Heres a list of your experience: #{exp}"
+        end
+        if skil.length != 0
+          puts "Heres a list of your skills: #{skil}"
+        end
+        if obst.length != 0
+          puts "Heres a list of your obstacles: #{obst}"
+        end
+        
+        
     end
 end
 
